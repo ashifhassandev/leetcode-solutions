7c3e63b37de82b8889b7806d402aba55f533646f
Ashif Hassan <ashifhassan1996@gmail.com>
2024-11-15 11:19:18 +0530

    New solution added

--- 9-palindrome-number/solution.js
+++ 9-palindrome-number/solution.js
@@ -1,5 +1,20 @@
 // Method 1
 const isPalindrome = (x) => {
+    if (x < 0 || x % 10 === 0 && x !== 0) {
+        return false;
+    }
+
+    let reversedHalf = 0;
+    while (x > reversedHalf) {
+        reversedHalf = reversedHalf * 10 + x % 10;
+        x = Math.floor(x / 10);
+    }
+
+    return x === reversedHalf || x === Math.floor(reversedHalf / 10);
+};
+
+// Method 2
+const checkPalindrome = (x) => {
     const reversed = x.toString().split('').reverse().join('');
     return x.toString() === reversed;
 };

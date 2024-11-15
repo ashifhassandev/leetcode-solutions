b3f21967681ddae6f73ea5968e0c4e9a0bb21bb1
Ashif Hassan <ashifhassan1996@gmail.com>
2024-11-15 15:22:13 +0530

    Add new problem

---
+++ 35-search-insert-position/solution.js
@@ -0,0 +1,10 @@
+// Method 1
+const searchInsert = (nums, target) => {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] >= target) {
+            return i;
+        }
+    }
+
+    return nums.length;
+};

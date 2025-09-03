2018f676619687888adc99a44921be580375fd17
Ashif Hassan <ashifhassan1996@gmail.com>
2025-09-03 13:07:35 +0530

    update two sum solution

--- top-100-liked/1-two-sum/twoSum.js
+++ top-100-liked/1-two-sum/twoSum.js
@@ -2,12 +2,11 @@
 const findTwoSum = (nums, target) => {
   for (let i = 0; i < nums.length - 1; i++) {
     for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
-      }
+      if (nums[i] + nums[j] === target) return [i, j];
     }
   }
 };
+
 
 // Method 2: less than 0(n2) time complexity
 const twoSum = (nums, target) => {
@@ -16,10 +15,7 @@ const twoSum = (nums, target) => {
   for (let i = 0; i < nums.length; i++) {
     const difference = target - nums[i];
 
-    if (map.has(difference)) {
-      return [map.get(difference), i];
-    }
-
+    if (map.has(difference)) return [map.get(difference), i];
     map.set(nums[i], i);
   }
 };

eb5a640b3fa78eb5bc711d10fb2348022741b318
Ashif Hassan <ashifhassan1996@gmail.com>
2025-09-07 09:01:35 +0530

    update remove duplicates from sorted array problem

--- top-interview-150/26-remove-duplicates-from-sorted-array/removeDuplicatesFromSortedArray.js
+++ top-interview-150/26-remove-duplicates-from-sorted-array/removeDuplicatesFromSortedArray.js
@@ -1,13 +1,47 @@
-// Method 1
+// Method 1:
 const removeDuplicates = (nums) => {
-    let k = 1;
+  let i;
 
-    for (let i = 0; i < nums.length; i++) {
-        if (i > 0 && nums[i] !== nums[i - 1]) {
-            nums[k] = nums[i];
-            k++;
-        }
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      let j = i;
+
+      while (j < nums.length - 1) {
+        nums[j] = nums[j + 1];
+        j++;
+      }
+
+      nums.length--;
+      i--;
     }
+  }
 
-    return k;
+  return i;
+};
+
+
+// Method 2:
+const removeDuplicateValues = (nums) => {
+  let k = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== nums[i + 1]) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+
+  return k;
+};
+
+
+// Method 3;
+const removeDuplicateElements = (nums) => {
+  const unique = [...new Set(nums)];
+
+  for (let i = 0; i < unique.length; i++) {
+    nums[i] = unique[i];
+  }
+
+  return unique.length;
 };

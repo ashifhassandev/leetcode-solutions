0d4ed7392ee0d5fbea5904f87dd793f669773786
Ashif Hassan <ashifhassan1996@gmail.com>
2025-09-09 13:57:28 +0530

    add remove element problem

--- top-interview-150/27-remove-element/removeElement.js
+++ top-interview-150/27-remove-element/removeElement.js
@@ -1,11 +1,33 @@
-// Method 1
+// Method 1:
 const removeElement = (nums, val) => {
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === val) {
-            nums.splice(i, 1);
-            i--;
-        }
-    }
+  let i = 0;
 
-    return nums.length;
+  while (i < nums.length) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+    } else {
+      i++;
+    }
+  }
+
+  return nums.length;
+};
+
+
+// Method 2:
+const removeElements = (nums, val) => {
+  let i = 0;
+  let j = nums.length - 1;
+
+  while (i <= j) {
+    if (nums[i] === val && nums[j] !== val) {
+      nums[i++] = nums[j--];
+    } else if (nums[i] === val && nums[j] === val) {
+      j--;
+    } else {
+      i++;
+    }
+  }
+
+  return i;
 };

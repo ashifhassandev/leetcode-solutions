4a13cabd54c966dcc9bf7374ff184ec298f880f3
Ashif Hassan <ashifhassan1996@gmail.com>
2025-09-09 14:08:39 +0530

    update remove element problem

--- top-interview-150/27-remove-element/removeElement.js
+++ top-interview-150/27-remove-element/removeElement.js
@@ -31,3 +31,18 @@ const removeElements = (nums, val) => {
 
   return i;
 };
+
+
+// Method 3:
+const removeArrayElement = (nums, val) => {
+  let k = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+
+  return k;
+};

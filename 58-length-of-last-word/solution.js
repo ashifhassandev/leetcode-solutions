9ecceff0670fdc5fa736ac8c29f51f5f30bd13da
Ashif Hassan <ashifhassan1996@gmail.com>
2025-09-12 09:59:34 +0530

    delete length of last word problem

--- 58-length-of-last-word/solution.js
+++
@@ -1,9 +0,0 @@
-// Method 1
-const lengthOfLastWord = (s) => {
-    const trimmed = s.trim();
-    const array = trimmed.split(' ');
-    
-    for (let i = 0; i < array.length; i++) {
-        return array[array.length - 1].length;
-    }
-};

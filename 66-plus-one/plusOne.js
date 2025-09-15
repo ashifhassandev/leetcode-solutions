18f1f0073bc8f5ea584bae8f11f9a5cec9a71369
Ashif Hassan <ashifhassan1996@gmail.com>
2025-09-15 11:57:57 +0530

    update plus one problem

---
+++ 66-plus-one/plusOne.js
@@ -0,0 +1,25 @@
+// Method 1:
+const plusOne = (digits) => {
+  const str = digits.join("");
+  const num = BigInt(str) + 1n;
+  return num
+    .toString()
+    .split("")
+    .map((num) => Number(num));
+};
+
+
+// Method 2:
+const addOne = (digits) => {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    }
+
+    digits[i] = 0;
+  }
+
+  digits.unshift(1);
+  return digits;
+};

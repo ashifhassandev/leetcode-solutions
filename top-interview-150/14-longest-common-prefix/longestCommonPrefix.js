1ee16410d1af33f111976cfb43eee4e64ec63d15
Ashif Hassan <ashifhassan1996@gmail.com>
2025-09-19 12:24:28 +0530

    add longest common prefix problem

---
+++ top-interview-150/14-longest-common-prefix/longestCommonPrefix.js
@@ -0,0 +1,16 @@
+// Method 1:
+const longestCommonPrefix = (strs) => {
+  let commonPrefix = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    let j = 0;
+
+    while (j < commonPrefix.length && strs[i][j] === commonPrefix[j]) {
+      j++;
+    }
+
+    commonPrefix = commonPrefix.substring(0, j);
+  }
+
+  return commonPrefix || "";
+};

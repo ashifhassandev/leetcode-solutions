93dd26f527eb4e1f3d8cd1ef4a0a64f579849fa7
Ashif Hassan <ashifhassan1996@gmail.com>
2025-09-24 10:27:44 +0530

    update two sum problem

--- top-100-liked/1-two-sum/twoSum.js
+++ top-100-liked/1-two-sum/twoSum.js
@@ -8,6 +8,19 @@ const findTwoSum = (nums, target) => {
 };
 
 
+// Method 2:
+const getTwoSum = (nums, target) => {
+  for (let i = 0; i < nums.length - 1; i++) {
+    let j = i + 1;
+
+    while (j < nums.length) {
+      if (nums[i] + nums[j] === target) return [i, j];
+      j++;
+    }
+  }
+};
+
+
 // Method 2: less than 0(n2) time complexity
 const twoSum = (nums, target) => {
   const map = new Map();

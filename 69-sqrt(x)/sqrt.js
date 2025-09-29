36bca1a4473568b58cd0c353077121bdcfe62caf
Ashif Hassan <ashifhassan1996@gmail.com>
2025-09-29 10:05:28 +0530

    update sqrt(x) problem

--- 69-sqrt(x)/sqrt.js
+++ 69-sqrt(x)/sqrt.js
@@ -1,2 +1,28 @@
 // Method 1
 const mySqrt = (x) => Math.floor(Math.sqrt(x));
+
+
+// Method 2:
+const findSqrt = (x) => {
+  if (x < 2) return x;
+
+  let left = 1;
+  let right = Math.floor(x / 2);
+  let nearest = 0;
+
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2);
+    const sq = middle * middle;
+
+    if (sq === x) return middle;
+
+    if (sq < x) {
+      nearest = middle;
+      left = middle + 1;
+    } else {
+      right = middle - 1;
+    }
+  }
+
+  return nearest;
+};

0c2d2ab36c03f6cc15ee893c74c275a4e6ca18cf
Ashif Hassan <ashifhassan1996@gmail.com>
2025-10-16 09:50:33 +0530

    add linked list cycle problem

---
+++ top-interview-150/141-linked-list-cycle/linkedListCycle.js
@@ -0,0 +1,14 @@
+// Method 1:
+const hasCycle = (head) => {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) return true;
+  }
+
+  return false;
+};

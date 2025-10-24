8c87e10940a3e9544dd8179c8a966eb4365c6f79
Ashif Hassan <ashifhassan1996@gmail.com>
2025-10-24 09:50:32 +0530

    add intersection of two arrays II problem

--- 350-intersection-of-two-arrays-II/intersectionOfTwoArraysII.js
+++ 350-intersection-of-two-arrays-II/intersectionOfTwoArraysII.js
@@ -25,3 +25,28 @@ const intersect = (nums1, nums2) => {
 
   return result;
 };
+
+
+// Method 2:
+const getIntersection = (nums1, nums2) => {
+  nums1.sort((a, b) => a - b);
+  nums2.sort((a, b) => a - b);
+
+  const result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < nums1.length && j < nums2.length) {
+    if (nums1[i] === nums2[j]) {
+      result.push(nums1[i]);
+      i++;
+      j++;
+    } else if (nums1[i] < nums2[j]) {
+      i++;
+    } else {
+      j++;
+    }
+  }
+
+  return result;
+};
